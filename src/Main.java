08b9fd48f189e8b9f7151052b98b208437a9a658
manny1892 <manny1892@gmail.com>
2022-04-22 21:44:22 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 120;
+        int moneyForMiles = 20;
+        int miles = price / moneyForMiles;
+
+        System.out.println(miles);
+
+    }
+}
